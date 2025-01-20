805c5d03765769b6165e168077fae9ac0440ee3c
Harshavardhan <hvardhan1024@gmail.com>
2025-01-20 22:03:40 +0530

    middleware added to handle errors

--- api/index.js
+++ api/index.js
@@ -8,21 +8,31 @@ dotenv.config()
 
 
 mongoose.connect(process.env.MONGO)
-.then(
-    ()=> console.log('connected')
-)
-.catch((err)=>console.log(err)
-)
+    .then(
+        () => console.log('connected')
+    )
+    .catch((err) => console.log(err)
+    )
 
 const app = express();
 app.use(express.json())
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server is running on port 3000!!');
-    
+
 })
 
-app.use('/api/user/',UserRoutes)
-app.use('/api/auth/',authRoutes)
+app.use('/api/user/', UserRoutes)
+app.use('/api/auth/', authRoutes)
 
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
