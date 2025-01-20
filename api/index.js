cce19ebb537982e2cea75380c9caa938aec0e4a1
Harshavardhan <hvardhan1024@gmail.com>
2025-01-20 20:22:38 +0530

    mongo db conection successful

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO)
+.then(
+    ()=> console.log('connected')
+)
+.catch((err)=>console.log(err)
+)
 
 const app = express();
 
